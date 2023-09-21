3f08ac7537946f99aeaa150039116089c3a5061a
sirken <sirken@users.noreply.github.com>
2023-09-21 10:04:39 -0600

    Display project name if active

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ def total_hours_today():
         api_response = api_instance.api_timesheets_get(modified_after=today)
         # print(api_response)
         total_seconds = 0
+        project_name = ''
         for timesheet in api_response:
             # completed entries with final duration (seconds)
             if timesheet.duration:
@@ -28,9 +29,16 @@ def total_hours_today():
             else:
                 # calculate time from when this entry began until now
                 total_seconds += (datetime.astimezone(datetime.now(tz=None)) - timesheet.begin).total_seconds()
+                project_id = f'{timesheet.project}'
+                try:
+                    api_project = kimai_python.ProjectApi(kimai_python.ApiClient(configuration))
+                    project = api_project.api_projects_id_get(project_id)
+                    project_name = f'{project.name[:5]}-'
+                except ApiException as e:
+                    print(f"Exception when calling api_projects_id_get: {e}\n")
         hr_min_sec = str(timedelta(seconds=total_seconds)).split(':')
         # return hours and minutes only
-        return f'{hr_min_sec[0]}:{hr_min_sec[1]}'
+        return f'{project_name}{hr_min_sec[0]}:{hr_min_sec[1]}'
     except ApiException as e:
         print(f"Exception when calling api_timesheets_get: {e}\n")
 
