aed295aa9a95be7c071380f3d8fafcb04a5fc691
sirken <sirken@users.noreply.github.com>
2023-11-28 13:20:48 -0700

    Only run on specific days of the week

--- kimai-hours.sh
+++ kimai-hours.sh
@@ -5,14 +5,19 @@ script="main.py"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR"
 
-if [ ! -d "venv" ]; then
-  echo "venv does not exist, creating"
-  python -m venv venv
-  source venv/bin/activate
-  pip install -r requirements.txt
-else
-  source venv/bin/activate
-fi
+# only run Mon (1) to Fri (5)
+if [ $(date +%u) -le 5 ]; then
+  if [ ! -d "venv" ]; then
+    echo "venv does not exist, creating"
+    python -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
+  else
+    source venv/bin/activate
+  fi
 
-python "$script"
-deactivate
+  python "$script"
+  deactivate
+else
+  echo ⚠
+fi
